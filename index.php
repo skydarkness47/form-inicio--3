8c499990ae148dcd8499d0e8d6a446346a036652
Jonathan Aguirre <yonathanaguirre@live.com>
2016-03-19 13:03:09 -0300

    creacion de INICION
    Se quito lo no necesario y agregaron dos botones de login y registro

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>FORMULARIO DE LOGIN JONATHAN AGUIRRE</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7; IE=EmulateIE9">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+    <link rel="shortcut icon" href=http://www.freshdesignweb.com/wp-content/themes/fv28/images/icon.ico />
+    <link rel="stylesheet" type="text/css" href="style.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="fdw-demo.css" media="all" />
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:700' rel='stylesheet' type='text/css'>
+   
+</head>
+<body>
+ <div class="container">
+      <div class="freshdesignweb-top"><h1><a href="https://github.com/skydarkness47/form-registro--1">PRIMER PROYECTO UTN</a></h1></div><div class="clr"></div>
+      <header>
+        <h1><span></span><strong> FORMULARIO DE LOGIN JONATHAN AGUIRRE</strong></h1>
+            </header>       
+      <div  class="form">
+        <form id="contactform"> 
+        
+        
+          <p align="center">  <input class="buttom" name="submit" id="submit" tabindex="5" value="REGISTRO" type="submit" onclick="comparar(x1,x2)">  &nbsp; &nbsp;&nbsp;&nbsp; <input class="buttom" name="reset" id ="reset" tabindex="5" type="reset"></input>
+</p>
+   </form> 
+  </div>       
+</div>
+         
+            
+</body>
+</html>
